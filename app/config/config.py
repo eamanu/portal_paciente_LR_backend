a4b94a5d40d622c257e5985ec596f8722664f8ad
Osvaldo Ignacio Ocanto <oocanto@conae.gov.ar>
2022-05-05 11:50:39 -0300

    Descarga de archivos de imágenes.

--- app/config/config.py
+++ app/config/config.py
@@ -1,5 +1,11 @@
 import os
 
+# region Base API names
+
+LR_BASE_API = "/portalpaciente/api/v1"
+
+# endregion
+
 # region openssl rand -hex 32
 
 SECRET_KEY = os.getenv("SECRET_KEY", "i-hate-w1nd0w$$")  # TODO: Remove this
@@ -19,8 +25,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # region Whitelisted Paths
 
 WHITE_LIST_PATH = (
-    "/portalpaciente/api/v1/login",
-    "/portalpaciente/api/v1/logout",
+    LR_BASE_API + "/login",
+    LR_BASE_API + "/logout",
     "/docs",
     "/openapi.json",
     "/favicon.ico",
@@ -51,21 +57,27 @@ LOCAL_FILE_DOWNLOAD_DIRECTORY = os.getenv("TMP_FILES_DOWNLOAD")
 # endregion
 
 # region mail configuration
+
 # TODO: Esto va a dejar de funcionar le 30 de mayo
 MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
 MAIL_FROM = os.getenv("MAIL_FROM", "no@mail.com")  # ConnectionConfig object need it as a valid mail format
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
 MAIL_PORT = os.getenv("MAIL_PORT", 123)
 MAIL_SERVER = os.getenv("MAIL_SERVER", "")
+
 # endregion
 
 # region validation-mail
+
 SERVER_IP = os.getenv("SERVER_IP", "http://127.0.0.1:8000")
-VALIDATION_ENDPOINT = os.getenv("VALIDATION_ENDPOINT", "/portalpaciente/api/v1/validate-email")
+VALIDATION_ENDPOINT = os.getenv("VALIDATION_ENDPOINT", LR_BASE_API + "/validate-email")
 VALIDATION_MAIL_URL = f"{SERVER_IP}{VALIDATION_ENDPOINT}"
+
 # endregion
 
-# regeion recover-password
-RECOVERY_ENDPOINT = os.getenv("RECOVERY_ENDPOINT", "/portalpaciente/api/v1/recovery-password")
+# region recover-password
+
+RECOVERY_ENDPOINT = os.getenv("RECOVERY_ENDPOINT", LR_BASE_API + "/recovery-password")
 RECOVERY_PASSWORD_URL = f"{SERVER_IP}{RECOVERY_ENDPOINT}"
+
 # endregion
