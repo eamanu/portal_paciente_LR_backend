7e0e2ef19fe74f4ee3664325052e7bf14063e75c
Emmanuel Arias <emmanuel.arias@onapsis.com>
2022-06-02 20:33:09 -0300

    disabling pool connections

--- app/config/database.py
+++ app/config/database.py
@@ -3,6 +3,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 from app.config.config import DATABASE_DEFAULT, DATABASE_URL
 
@@ -16,7 +17,7 @@ MYSQL = 'mysql'
 if DATABASE_DEFAULT == SQLITE:
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 else:
-    engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
+    engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 conn = engine.connect()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/gear/hsi/database.py
+++ app/gear/hsi/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 from app.gear.hsi.config import HSI_DATABASE_URL
 
-
-engine = create_engine(HSI_DATABASE_URL)
+engine = create_engine(HSI_DATABASE_URL, poolclass=NullPool)
 
 SessionLocalHSI = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
