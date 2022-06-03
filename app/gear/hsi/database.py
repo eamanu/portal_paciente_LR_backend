bf16aeb8a65339f3a5aa5deb24e0bf1145e9fc46
Emmanuel Arias <eamanu@yaerobi.com>
2022-06-03 16:20:08 -0300

    dont use nullpool in hsi database

--- app/gear/hsi/database.py
+++ app/gear/hsi/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
 
 from app.gear.hsi.config import HSI_DATABASE_URL
 
-engine = create_engine(HSI_DATABASE_URL, poolclass=NullPool)
+engine = create_engine(HSI_DATABASE_URL, pool_size=20, max_overflow=0)
 
 SessionLocalHSI = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
